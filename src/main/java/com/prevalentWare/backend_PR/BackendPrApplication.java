8f331ead5124c33c1bc6dae0bb0753cdc6710b8f
Cristian Castaño <cristiancastano852@gmail.com>
2023-12-06 21:15:26 -0500

    Init project

---
+++ src/main/java/com/prevalentWare/backend_PR/BackendPrApplication.java
@@ -0,0 +1,13 @@
+package com.prevalentWare.backend_PR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prevalentWare/backend_PR/BackendPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prevalentWare.backend_PR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
